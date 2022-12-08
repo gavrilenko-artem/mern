b9389f3fd2ff7269045430fc149f9694f8a6e6ac
Artem <0982605567artem@gmail.com>
2022-12-08 14:14:30 +0200

    add jsonwebtoken

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Holla!");
+});
+
+app.post("/auth/login", (req, res) => {
+  const token = jwt.sign(
+    {
+      email: req.body.email,
+    },
+    "secretkey"
+  );
+
+  res.json({
+    success: true,
+    token,
+  });
 });
 
 app.listen(4000, (err) => {
